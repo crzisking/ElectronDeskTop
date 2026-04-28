33753b4def4f9b9e681e9e1de796bf1d8cdf106e
jacky.chen <jacky.chen@ichia.com>
2026-04-28 16:53:10 +0800

    refactor(types): 移除未使用的 Element Plus 组件类型声明
    - 移除了 ElBadge 组件的类型声明
    - 移除了 ElCol 组件的类型声明
    - 移除了 ElDivider 组件的类型声明
    - 保留了项目中仍在使用的其他 Element Plus 组件类型
    - 清理了组件类型的冗余声明以优化类型定义文件

--- src/src/types/components.d.ts
+++ src/src/types/components.d.ts
@@ -8,14 +8,11 @@ export {}
 declare module 'vue' {
   export interface GlobalComponents {
     ElAlert: typeof import('element-plus/es')['ElAlert']
-    ElBadge: typeof import('element-plus/es')['ElBadge']
     ElButton: typeof import('element-plus/es')['ElButton']
     ElCard: typeof import('element-plus/es')['ElCard']
-    ElCol: typeof import('element-plus/es')['ElCol']
     ElDescriptions: typeof import('element-plus/es')['ElDescriptions']
     ElDescriptionsItem: typeof import('element-plus/es')['ElDescriptionsItem']
     ElDialog: typeof import('element-plus/es')['ElDialog']
-    ElDivider: typeof import('element-plus/es')['ElDivider']
     ElEmpty: typeof import('element-plus/es')['ElEmpty']
     ElForm: typeof import('element-plus/es')['ElForm']
     ElFormItem: typeof import('element-plus/es')['ElFormItem']
